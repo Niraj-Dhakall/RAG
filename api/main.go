0ff4157b72f102be3ceae7a140cdc81254f3cd05
Niraj Dhakal <yboii34@gmail.com>
2026-06-06 22:51:40 -0400

    feat: Completed handleSearch function. This function takes in a Query and a TopK value and outputs the result of the RAG.

--- api/main.go
+++ api/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func handleHealthCheck(w http.ResponseWriter, r *http.Request){
+type SearchRequest struct {
+	Query string `json:"query"`
+	TopK  int    `json:"topK"`
+}
+type server struct {
+	pool *pgxpool.Pool
+}
+
+func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allowed-Methods", "GET")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
@@ -22,16 +34,49 @@ func handleHealthCheck(w http.ResponseWriter, r *http.Request){
 	})
 }
 
-func handleSearch(w http.ResponseWriter, r *http.Request){
-	
+func (s *server) handleSearch(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allowed-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+	var request SearchRequest
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		fmt.Printf("Error Decoding request %v: \n", err)
+		http.Error(w, "Error decoding request", http.StatusBadRequest)
+	}
+
+	embedding, err := embeddingGenerator(r.Context(), request.Query)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	results, err := similaritySearch(r.Context(), s.pool, embedding, request.TopK)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]interface{}{"Results": results})
 }
 func main() {
-	http.HandleFunc("/health", handleHealthCheck)
-	http.HandleFunc("/search", handleSearch)
+	pool, err := connectDB(context.Background())
+	if err != nil {
+		log.Printf("Error connecting with DB: %v", err)
+	}
 
-	err := http.ListenAndServe(":8080", nil)
+	s := &server{pool: pool}
+	http.HandleFunc("/health", handleHealthCheck)
+	http.HandleFunc("/search", s.handleSearch)
+
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("Error starting server: %s", err)
 	}
-	
+
 }
